439d9175cd1651c75be2333d09dd8c9052f1d84c
Jeffrey <jeffdbhk@gmail.com>
2019-08-06 13:47:56 -0700

    portfolio items

---
+++ site/js/AboutMe.js
@@ -0,0 +1,41 @@
+// Close the dropdown if the user clicks outside of it
+
+var lis = document.querySelectorAll("a");
+
+for(var i = 0; i < lis.length; i++){
+   //console.log(this.id);
+   lis[i].addEventListener("mouseover", function(){
+      console.log("mouseover");
+       this.id="selected";
+       
+   });
+
+   lis[i].addEventListener("mouseout", function(){
+    console.log("mouseout");
+       this.id="deselected";
+   });
+
+   lis[i].addEventListener("click", function(){
+      console.log("clicked");
+   });
+}
+$('a').on("mouseover", function(e) {
+  if (e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+});   
+$('#Skillset').click(function()
+                     {$('.bar').each(function(){
+                         var fill =$(this).find(".fill");
+                         var ammount = fill.data("width");
+                         fill.css("width", ammount);
+                     });
+    
+})
---
+++ site/js/paintings.js
@@ -0,0 +1,31 @@
+var lis = document.querySelectorAll("a");
+
+for(var i = 0; i < lis.length; i++){
+   //console.log(this.id);
+   lis[i].addEventListener("mouseover", function(){
+      console.log("mouseover");
+       this.id="selected";
+       
+   });
+
+   lis[i].addEventListener("mouseout", function(){
+    console.log("mouseout");
+       this.id="deselected";
+   });
+
+   lis[i].addEventListener("click", function(){
+      console.log("clicked");
+   });
+}
+$('a').on("mouseover", function(e) {
+  if (e.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var d = 0; d < dropdowns.length; d++) {
+      var openDropdown = dropdowns[d];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+});   
